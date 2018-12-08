fa7678bb5964d35d164264d39fb1983464b62bbd
S <s>
2018-12-08 21:44:26 +0400

    {"types":"C","runs":0}

--- TestYahtzeeDisplay.java
+++ TestYahtzeeDisplay.java
@@ -1,4 +1,18 @@
+import acm.graphics.GCanvas;
+import acm.program.GraphicsProgram;
 
-public class TestYahtzeeDisplay {
-
+public class TestYahtzeeDisplay extends GraphicsProgram {
+	// Constructor of class
+	public TestYahtzeeDisplay() {
+		
+	}
+	
+	public TestYahtzeeDisplay(GCanvas canvas, String[] starray) {
+		this.canvas = canvas;
+		this.names = starray;
+	}
+	
+	
+	private GCanvas canvas;
+	private String[] names;
 }
