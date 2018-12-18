97e90b5d756430a471ca2ef05bc7541da0a0d00a
S <s>
2018-12-18 20:57:41 +0400

    {"types":"C","runs":11}

--- CategoryChecker.java
+++ CategoryChecker.java
@@ -1,4 +1,43 @@
 
 public class CategoryChecker implements YahtzeeConstants {
 	
+	
+	
+	private boolean isOneToSix(int[] array, int category) {
+		return true;
+	}
+	
+	private boolean isThreeOfAKind(int[] array, int category) {
+		return false;
+		
+	}
+	
+	private boolean isFourOfAKind(int[] array, int category) {
+		return false;
+		
+	}
+	
+	private boolean isSmallStraight(int[] array, int category) {
+		return false;
+		
+	}
+	
+	private boolean isBigStraight(int[] array, int category) {
+		return false;
+		
+	}
+	
+	private boolean isYahtzee(int[] array, int category) {
+		return false;
+		
+	}
+	
+	private boolean isChance(int[] array, int category) {
+		return true;
+		
+	}
+	
+	private int straightCounter(int[] arr) {
+		return 0;
+	}
 }
