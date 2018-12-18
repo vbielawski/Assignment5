260e4030015e948684667ad7ce109f5b7bcc1682
S <s>
2018-12-18 21:15:41 +0400

    {"types":"C","runs":12}

--- ScoresCounter.java
+++ ScoresCounter.java
@@ -1,4 +1,12 @@
 
-public class ScoresCounter {
+public class ScoresCounter implements YahtzeeConstants{
 
+	private static final int FullHouseScore = 25;
+	private static final int SmallStraightScore = 30;
+	private static final int BigStraightScore = 40;
+	private static final int YahtzeeScore = 50;
+	
+	CategoryChecker ct = new CategoryChecker();
+	
+	
 }
