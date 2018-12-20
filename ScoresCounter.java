cf5e966ea956ec9522cda1fda0e68ab3400f9d12
S <s>
2018-12-20 15:56:16 +0400

    {"types":"C","runs":0}

--- ScoresCounter.java
+++ ScoresCounter.java
@@ -7,12 +7,62 @@ public class ScoresCounter implements YahtzeeConstants {
 	public int YahtzeeScore = 50;
 	
 
-	public int getScoreOneSix(boolean isCategory, int category, int[] array) {
+	public int getScoreOneSix(int category,int[] array) {
 		boolean onetosixfound = categoryfounder(array, category);
 		int sum = 0;
 		if(onetosixfound) {
 			
 		}
+		return sum;
+	}
+	
+	public int getFourOfAKindScore(int[] array) {
+		int sum = 0;
+		return sum;
+	}
+	
+	public int getFullHouseScore(int[] array) {
+		int sum = 0;
+		return sum;
+	}
+	
+	public int getSmallStraightScore(int[] array) {
+		int sum = 0;
+		return sum;
+	}
+	
+	public int getLargeStraightScore(int[] array) {
+		int sum = 0;
+		return sum;
+	}
+	
+	public int getYahtzeeScore(int[] array) {
+		int sum = 0;
+		return sum;
+	}
+	
+	public int getChanceScore(int[] array) {
+		int sum = 0;
+		return sum;
+	}
+	
+	public int getUpperScore(int[] array) {
+		int sum = 0;
+		return sum;
+	}
+	
+	public int getLowerScore(int[] array) {
+		int sum = 0;
+		return sum;
+	}
+	
+	public int getUpperBonusScore(int[] array) {
+		int sum = 0;
+		return sum;
+	}
+	
+	public int getTotalScore(int[] array) {
+		int sum = 0;
 		return sum;
 	}
 
