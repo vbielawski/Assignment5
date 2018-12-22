d3ad58c550bbfa437913885279bc013c69801ab6
S <s>
2018-12-23 00:28:01 +0400

    {"types":"C","runs":13}

--- CategoryChecker.java
+++ CategoryChecker.java
@@ -73,28 +73,28 @@ public class CategoryChecker implements YahtzeeConstants {
 	public boolean isSmallStraight(int[] array, int category) {
 		sortArray(array);
 		int counter = 0;
-		for(int i = 0; i < array.length - 1; i++) {
-			if(isStraight(i, i+1, array)) {
+		for (int i = 0; i < array.length - 1; i++) {
+			if (isStraight(i, i + 1, array)) {
 				counter++;
 			}
 		}
-		if(counter >= 4) {
+		if (counter >= 4) {
 			return true;
 		}
-		
+
 		return false;
 
 	}
 
-	public boolean isLargeStraight(int[] array, int category) {
+	public boolean isLargeStraight(int[] array) {
 		sortArray(array);
 		int counter = 0;
-		for(int i = 0; i < array.length - 1; i++) {
-			if(isStraight(i, i+1, array)) {
+		for (int i = 0; i < array.length - 1; i++) {
+			if (isStraight(i, i + 1, array)) {
 				counter++;
 			}
 		}
-		if(counter == 5) {
+		if (counter == 5) {
 			return true;
 		}
 		return false;
@@ -102,7 +102,12 @@ public class CategoryChecker implements YahtzeeConstants {
 	}
 
 	public boolean isYahtzee(int[] array, int category) {
-		return false;
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] != array[0]) {
+				return false;
+			}
+		}
+		return true;
 
 	}
 
@@ -115,30 +120,29 @@ public class CategoryChecker implements YahtzeeConstants {
 		boolean isPlusOne = (arr[index2] == arr[index1] + 1);
 		return isPlusOne;
 	}
-	
+
 	private void sortArray(int[] arr) {
-		for(int i = 0; i < arr.length; i++) {
-			int min = findSmallest(arr, i, arr.length); 
-			swapElements(arr, i, min);	
-			}
+		for (int i = 0; i < arr.length; i++) {
+			int min = findSmallest(arr, i, arr.length);
+			swapElements(arr, i, min);
 		}
-	
-	
+	}
+
 	private int findSmallest(int[] array, int p1, int p2) {
 		int smallestIndex = p1;
-		for(int i = p1 + 1; i < p2; i++) {
-			if (array[i] < array[smallestIndex]) smallestIndex = i;
+		for (int i = p1 + 1; i < p2; i++) {
+			if (array[i] < array[smallestIndex])
+				smallestIndex = i;
 		}
+
 		return smallestIndex;
 	}
-	
+
 	private void swapElements(int[] array, int p1, int p2) {
 		int temp = array[p1];
 		array[p1] = array[p2];
 		array[p2] = temp;
 	}
-	
-	
 
 	private int frequencylength = 7;
 	private int[] frequencyTable;
