bf17fa8e69e4becc3b70384873e22bfb3afcdce8
S <s>
2018-12-22 18:29:56 +0400

    {"types":"C","runs":2}

--- CategoryChecker.java
+++ CategoryChecker.java
@@ -1,15 +1,40 @@
+import java.io.Console;
+
+import acm.io.IOConsole;
+import acm.io.IODialog;
 
 public class CategoryChecker implements YahtzeeConstants {
 	
+	public void main(String[] args) {
+		IODialog dial = new IODialog();
+		IOConsole cons = new IOConsole();
+		int[] array = new int[5];
+		for(int i = 0; i < array.length; i++) {
+			array[i] = dial.readInt("number: ");
+		}
+		
+		for(int el : array) {
+			cons.print(el);
+		}
+		
+	}
 	
 	
+	private int[] initialize() {
+		int[] arr = new int[frequencylength];
+		for(int i = 0; i < arr.length; i++) {
+			arr[i] = 0;
+		}
+		return arr;
+		
+	}
 	
 	public boolean isOneToSix(int[] array, int category) {
-		
 		return true;
 	}
 	
 	public boolean isThreeOfAKind(int[] array, int category) {
+		
 		return false;
 		
 	}
@@ -39,6 +64,6 @@ public class CategoryChecker implements YahtzeeConstants {
 		
 	}
 	
-	
+	private int frequencylength = 7;
 	
 }
