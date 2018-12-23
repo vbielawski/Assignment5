c2f7618860d7baddf4ff2d09c93d7c953a80224f
S <s>
2018-12-23 16:54:00 +0400

    {"types":"C","runs":11}

--- ScoresCounter.java
+++ ScoresCounter.java
@@ -1,88 +1,118 @@
 
 public class ScoresCounter implements YahtzeeConstants {
 
-	public int FullHouseScore = 25;
-	public int SmallStraightScore = 30;
-	public int BigStraightScore = 40;
-	public int YahtzeeScore = 50;
-	
+	private int FullHouseScore = 25;
+	private int SmallStraightScore = 30;
+	private int LargeStraightScore = 40;
+	private int YahtzeeScore = 50;
 
-	public int getScoreOneSix(int category,int[] array) {
-		boolean onetosixfound = categoryfounder(array, category);
+	public int getScoreOneSix(int category, int[] array) {
 		int sum = 0;
-		if(onetosixfound) {
-			
+		switch (category) {
+		case ONES:
+			for (int i = 0; i < array.length; i++) {
+				if (array[i] == 1)
+					sum++;
+			}
+		case TWOS:
+			for (int i = 0; i < array.length; i++) {
+				if (array[i] == 2)
+					sum += i;
+			}
+		case THREES:
+			for (int i = 0; i < array.length; i++) {
+				if (array[i] == 3)
+					sum += i;
+			}
+		case FOURS:
+			for (int i = 0; i < array.length; i++) {
+				if (array[i] == 4)
+					sum += i;
+			}
+		case FIVES:
+			for (int i = 0; i < array.length; i++) {
+				if (array[i] == 5)
+					sum += i;
+			}
+		case SIXES:
+			for (int i = 0; i < array.length; i++) {
+				if (array[i] == 6)
+					sum += i;
+			}
 		}
 		return sum;
 	}
-	
+
 	public int getThreeOfAKindScore(int[] array) {
 		int sum = 0;
+		for (int i = 0; i < array.length; i++) {
+			sum += array[i];
+		}
 		return sum;
 	}
-	
+
 	public int getFourOfAKindScore(int[] array) {
 		int sum = 0;
+		for (int i = 0; i < array.length; i++) {
+			sum += array[i];
+		}
 		return sum;
 	}
-	
-	public int getFullHouseScore(int[] array) {
-		int sum = 0;
-		return sum;
+
+	public int getFullHouseScore() {
+
+		return FullHouseScore;
 	}
-	
-	public int getSmallStraightScore(int[] array) {
-		int sum = 0;
-		return sum;
+
+	public int getSmallStraightScore() {
+
+		return SmallStraightScore;
 	}
-	
+
 	public int getLargeStraightScore(int[] array) {
-		int sum = 0;
-		return sum;
+
+		return LargeStraightScore;
 	}
-	
+
 	public int getYahtzeeScore(int[] array) {
-		int sum = 0;
-		return sum;
+
+		return YahtzeeScore;
 	}
-	
+
 	public int getChanceScore(int[] array) {
 		int sum = 0;
+		for (int i = 0; i < array.length; i++) {
+			sum += array[i];
+		}
 		return sum;
 	}
-	
+
 	public int getUpperScore(int[] array) {
 		int sum = 0;
 		return sum;
 	}
-	
+
 	public int getLowerScore(int[] array) {
 		int sum = 0;
 		return sum;
 	}
-	
+
 	public int getUpperBonusScore(int[] array) {
 		int sum = 0;
 		return sum;
 	}
-	
+
 	public int getTotalScore(int[] array) {
 		int sum = 0;
 		return sum;
 	}
 
-
 	private boolean categoryfounder(int[] array, int category) {
 		// TODO Auto-generated method stub
-		switch(category) {
-			
+		switch (category) {
+
 		}
 		return false;
 	}
 
-
-	
-
-
-	
 }
